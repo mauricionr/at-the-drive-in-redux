194d08aea54be57568290f84ef2f72d1dfb0d431
Mike Wilcox <mwilcox56@gmail.com>
2015-06-27 17:29:22 -0400

    add a basic server

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var exec = require('child_process').exec;
+
+const api = express()
+  .get('/torrent-stream/:magnet?', function(req, res) {
+    var cmd = exec('peerflix "' + req.query.magnet + '" --vlc', function (error, stdout, stderr) {
+        console.log('kk', stdout);
+    });
+
+    res.send({ data: 'torrent data' })
+  });
+
+const app = express()
+  .all('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+    next()
+  });
+
+app.use('/api', api)
+  .listen(3000, () => console.log('listening on port 3000'));
