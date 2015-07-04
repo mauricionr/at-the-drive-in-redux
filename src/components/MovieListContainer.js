76fcf8264ee2d8ab49193e5d3326893555ce02ec
Mike Wilcox <mwilcox56@gmail.com>
2015-07-04 16:47:35 -0400

    add MovieListContainer to fetch all the stuff

---
+++ src/components/MovieListContainer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import MovieList from './MovieList';
+import MoviesAPI from '../utils/API';
+
+export default class Root extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = { movies: [] }
+  }
+
+  componentDidMount() {
+
+    let moviesAPI = new MoviesAPI();
+
+    moviesAPI.getMovies().then((movies) => {
+      this.setState({ movies });
+    })
+  }
+
+  render() {
+    return (
+      <MovieList movies={this.state.movies} />
+    )
+  }
+
+}
