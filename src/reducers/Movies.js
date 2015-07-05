801eebb8dbd3e4f4fe28d51beae420154a9432a0
Mike Wilcox <mwilcox56@gmail.com>
2015-07-05 17:13:03 -0400

    add currStream to initialState

--- src/reducers/Movies.js
+++ src/reducers/Movies.js
@@ -2,7 +2,7 @@ import Immutable from 'immutable';
 import ActionTypes from '../consts/ActionTypes';
 import createReducer from '../lib/createReducer';
 
-const initialState = Immutable.fromJS({movies: []});
+const initialState = Immutable.fromJS({movies: [], currStream: ''});
 
 export default createReducer(initialState, {
   [ActionTypes.Movies.getMovies](state, { res }) {
