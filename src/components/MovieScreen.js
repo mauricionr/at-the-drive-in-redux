36b42fde3a03a0187808640a3de1a3ec9de0b90e
Mike Wilcox <mwilcox56@gmail.com>
2015-07-05 17:12:41 -0400

    connect reducer to component -> stream movie

--- src/components/MovieScreen.js
+++ src/components/MovieScreen.js
@@ -1,15 +1,27 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'redux/react';
 
-// TODO: get url to stream movie from reducer
-
-export default class MovieScreen {
+@connect(({ Movies }) => ({ Movies }))
+export default class MovieScreen extends React.Component {
 
   render() {
+
+    const {
+      props: {
+        Movies
+      }
+    } = this;
+
+    const currStream = Movies.get(`currStream`);
+
     return (
       <div>
-        <Link to="/">Back</Link>
-        <video width="900" height="450" src="https://ia802700.us.archive.org/22/items/Drive-inWelcomeToTheDrive-in/DriveIn-WelcomeToTheDriveIn-NightSky_512kb.mp4" preload="auto" controls></video>
+        <Link className="btn-back" to="/">Back</Link>
+        { currStream !== '' ?
+            <video width="900" height="450" src={currStream} preload="auto" controls></video>:
+            <video width="900" height="450" src="https://ia802700.us.archive.org/22/items/Drive-inWelcomeToTheDrive-in/DriveIn-WelcomeToTheDriveIn-NightSky_512kb.mp4" preload="auto" controls></video>
+        }
       </div>
     );
   }
