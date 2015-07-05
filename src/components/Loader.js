f580cb730b3d34ab0e0620bbc02e5e51d101a203
Mike Wilcox <mwilcox56@gmail.com>
2015-07-05 12:48:14 -0400

    update teh loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,22 +4,10 @@ export default class MovieScreen {
 
   render() {
     return (
-      <div>
-        <div id="cupcake" className="box">
-          <span className="letter">L</span>
-
-            <div className="cupcakeCircle box">
-              <div className="cupcakeInner box">
-                <div className="cupcakeCore box"></div>
-              </div>
-            </div>
-
-            <span className="letter box">A</span>
-            <span className="letter box">D</span>
-            <span className="letter box">I</span>
-            <span className="letter box">N</span>
-            <span className="letter box">G</span>
-        </div>
+      <div className="circles">
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     );
   }
