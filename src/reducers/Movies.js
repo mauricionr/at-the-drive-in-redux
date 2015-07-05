563fb7d129ca8608204f7d150dbea8fcec44176a
Mike Wilcox <mwilcox56@gmail.com>
2015-07-05 12:23:39 -0400

    add handler for watchMovie

--- src/reducers/Movies.js
+++ src/reducers/Movies.js
@@ -2,13 +2,18 @@ import Immutable from 'immutable';
 import ActionTypes from '../consts/ActionTypes';
 import createReducer from '../lib/createReducer';
 
-const initialState = Immutable.fromJS({});
+const initialState = Immutable.fromJS({movies: []});
 
 export default createReducer(initialState, {
-
-  [ActionTypes.Movies.getMovies](state, { movies, res }) {
+  [ActionTypes.Movies.getMovies](state, { res }) {
     return state.merge({
-      [movies]: res.body
+      [`movies`]: res
+    });
+  },
+
+  [ActionTypes.Movies.watchMovie](state, { res }) {
+    return state.merge({
+      [`currStream`]: res.address
     });
   }
 });
