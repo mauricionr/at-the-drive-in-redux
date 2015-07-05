52dee46ff17629136ad8c55d3bcead412744ce86
Mike Wilcox <mwilcox56@gmail.com>
2015-07-05 12:21:13 -0400

    pass down bound action creators to child

--- src/components/MovieListContainer.js
+++ src/components/MovieListContainer.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import MovieList from './MovieList';
 import MoviesAPI from '../utils/API';
+import prepareRoute from '../decorators/prepareRoute';
+import * as MoviesActionCreators from '../actions/movies';
+import { connect } from 'redux/react';
+import {bindActionCreators} from 'redux';
+import * as moviesActions from '../actions/movies';
 
+@prepareRoute(async ({ redux, params: { } }) => {
+  return await * [
+    redux.dispatch(MoviesActionCreators.getMovies())
+  ];
+})
+@connect(({ Movies }) => ({ Movies }))
 export default class Root extends React.Component {
 
-  constructor(props, context) {
-    super(props, context);
-    this.state = { movies: [] }
-  }
-
-  componentDidMount() {
-
-    let moviesAPI = new MoviesAPI();
-
-    moviesAPI.getMovies().then((movies) => {
-      this.setState({ movies });
-    })
-  }
-
   render() {
+    const {
+      props: {
+        Movies
+      }
+    } = this;
+    
+    const movies = Movies.get(`movies`).toJS();
+
     return (
-      <MovieList movies={this.state.movies} />
+      <MovieList movies={movies} {...bindActionCreators(moviesActions, this.props.dispatch)} />
     )
   }
 
