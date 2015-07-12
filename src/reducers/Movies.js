8616f79fb56d9ae27e494063b448f179e43a87f6
Mike Wilcox <mwilcox56@gmail.com>
2015-07-12 18:22:51 -0400

    handle paging

--- src/reducers/Movies.js
+++ src/reducers/Movies.js
@@ -2,12 +2,13 @@ import Immutable from 'immutable';
 import ActionTypes from '../consts/ActionTypes';
 import createReducer from '../lib/createReducer';
 
-const initialState = Immutable.fromJS({movies: [], currStream: ''});
+const initialState = Immutable.fromJS({page: 1, movies: [], currStream: ''});
 
 export default createReducer(initialState, {
   [ActionTypes.Movies.getMovies](state, { res }) {
     return state.merge({
-      [`movies`]: res
+      [`movies`]: res.movies,
+      [`page`]: res.page
     });
   },
 
