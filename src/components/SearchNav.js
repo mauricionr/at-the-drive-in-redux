b8f79db7751ff634ef53fb70effa91354bfe5ace
Mike Wilcox <mwilcox56@gmail.com>
2015-07-12 18:22:37 -0400

    add search nav component

---
+++ src/components/SearchNav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class SearchNav extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  _next() {
+    this.props.getMovies({
+      page: this.props.redux.getState().Movies.toJS().page,
+      motion: 1
+    })
+  }
+
+  _last() {
+    this.props.getMovies({
+      page: this.props.redux.getState().Movies.toJS().page,
+      motion: -1
+    })
+  }
+
+  render() {
+    return (
+      <div className="search-nav">
+        <button onClick={this._last.bind(this)}>last</button>
+        <button className="right" onClick={this._next.bind(this)}>next</button>
+      </div>
+    );
+  }
+
+}
