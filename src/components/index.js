113adc31980de5671ea4e7bfda808ab79fb56e21
Mike Wilcox <mwilcox56@gmail.com>
2015-07-19 16:52:34 -0400

    add header

--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,4 @@ export { default as MovieScreen } from './MovieScreen'
 export { default as Loader } from './Loader'
 export { default as Search } from './Search'
 export { default as SearchNav } from './SearchNav'
+export { default as Header } from './Header';
