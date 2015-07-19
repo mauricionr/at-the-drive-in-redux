d6fad931238cb36f3c5358eacc507808e49cc397
Mike Wilcox <mwilcox56@gmail.com>
2015-07-19 16:51:43 -0400

    add support for genre querying

--- src/actions/movies.js
+++ src/actions/movies.js
@@ -4,10 +4,12 @@ export function getMovies(query) {
 
   let page = query.motion ? query.page += query.motion : query.page;
 
+  let genre = query.genre ? `?genre=${query.genre}` : ``;
+
   return async api => ({
     type: ActionTypes.Movies.getMovies,
     res: {
-      movies: await api(`/movies?page=${query.page}`),
+      movies: await api(`/movies?page=${query.page}${genre}`),
       page: page
     }
   });
