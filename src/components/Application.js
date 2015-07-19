8225475f56d0feb11e44739fb5306c85a234a5d2
Mike Wilcox <mwilcox56@gmail.com>
2015-07-19 16:52:20 -0400

    pass action handlers down to header

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Header } from './index';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as moviesActions from '../actions/movies';
 
+@connect(({ }) => ({ }))
 class Application extends React.Component {
 
   render() {
     const { props: { children } } = this;
 
     return (
-      <div className="Application">
+      <div className="container">
+        <Header {...bindActionCreators(moviesActions, this.props.dispatch)} />
         {children}
       </div>
     );
