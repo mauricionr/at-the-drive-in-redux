0cb03c7e2e95ae06182ce568f4a6821c73e9bb65
Mike Wilcox <mwilcox56@gmail.com>
2015-07-19 21:09:07 -0400

    formatting query

--- src/actions/movies.js
+++ src/actions/movies.js
@@ -2,14 +2,14 @@ import ActionTypes from '../consts/ActionTypes';
 
 export function getMovies(query) {
 
-  let page = query.motion ? query.page += query.motion : query.page;
+  let page = query.motion ? `?page=${query.page += query.motion}` : `?page=${query.page}`;
 
-  let genre = query.genre ? `?genre=${query.genre}` : ``;
+  let genre = query.genre ? `&genre=${query.genre}` : ``;
 
   return async api => ({
     type: ActionTypes.Movies.getMovies,
     res: {
-      movies: await api(`/movies?page=${query.page}${genre}`),
+      movies: await api(`/movies${page}${genre}`),
       page: page
     }
   });
