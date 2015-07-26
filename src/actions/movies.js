c8b1bab3a1289c39403a2a2313238aa2b92cd27a
Mike Wilcox <mwilcox56@gmail.com>
2015-07-26 13:18:11 -0400

    add moar movies

--- src/actions/movies.js
+++ src/actions/movies.js
@@ -1,17 +1,49 @@
 import ActionTypes from '../consts/ActionTypes';
 
 export function getMovies(query) {
-
-  let page = query.motion ? `?page=${query.page += query.motion}` : `?page=${query.page}`;
   let genre = query.genre ? `&genre=${query.genre}` : ``;
   let sort = query.sort ? `&sort=${query.sort}` : ``;
 
   return async api => ({
     type: ActionTypes.Movies.getMovies,
     res: {
-      movies: await api(`/movies${page}${genre}${sort}`),
-      page: page,
-      genre: genre
+      movies: await api(`/movies?page=1${genre}${sort}`),
+      page: 1,
+      filtering: {
+        sort: {
+          on: sort !== ``,
+          value: query.sort
+        },
+        genre: {
+          on: genre !== ``,
+          value: query.genre
+        }
+      }
+    }
+  });
+}
+
+export function getMoreMovies(query) {
+  let pageNum = query.page += query.motion;
+  let pageQuery = `?page=${pageNum}`;
+  let genre = query.genre ? `&genre=${query.genre}` : ``;
+  let sort = query.sort ? `&sort=${query.sort}` : ``;
+
+  return async api => ({
+    type: ActionTypes.Movies.getMovies,
+    res: {
+      movies: await api(`/movies${pageQuery}${genre}${sort}`),
+      page: pageNum,
+      filtering: {
+        sort: {
+          on: sort !== ``,
+          value: query.sort
+        },
+        genre: {
+          on: genre !== ``,
+          value: query.genre
+        }
+      }
     }
   });
 }
@@ -26,6 +58,9 @@ export function watchMovie(magnet) {
 export function searchMovie(query) {
   return async api => ({
     type: ActionTypes.Movies.searchMovie,
-    res: await api(`/search?movie=${query}`)
+    res: {
+      movies: await api(`/search?movie=${query}`),
+      page: 1
+    }
   })
 }
