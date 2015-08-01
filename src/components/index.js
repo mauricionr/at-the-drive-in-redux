4a22208c1478720c1424aa5994c83f6b2e633072
Mike Wilcox <mwilcox56@gmail.com>
2015-08-01 10:09:27 -0400

    tidy

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,13 @@
-export { default as Application } from './Application'
+export { default as Application } from './App/Application'
+export { default as Router } from './App/Router'
 
-export { default as MovieListContainer } from './MovieListContainer'
-export { default as MovieList } from './MovieList'
-export { default as MovieScreen } from './MovieScreen'
+export { default as MovieListContainer } from './Movies/MovieListContainer'
+export { default as MovieList } from './Movies/MovieList'
+export { default as MovieScreen } from './Movies/MovieScreen'
+export { default as Search } from './Movies/Search'
+export { default as SearchNav } from './Movies/SearchNav'
 
-export { default as Loader } from './Loader'
-export { default as Search } from './Search'
-export { default as SearchNav } from './SearchNav'
-export { default as Header } from './Header';
+export { default as ShowListContainer } from './Shows/ShowListContainer'
+
+export { default as Loader } from './Common/Loader'
+export { default as Header } from './Common/Header';
