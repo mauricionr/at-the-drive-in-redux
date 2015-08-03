25bbf9c0a7c6a3fcae83c2044494b39cd0088c67
Mike Wilcox <mwilcox56@gmail.com>
2015-08-02 21:52:40 -0400

    add shows route

--- src/components/App/Router.js
+++ src/components/App/Router.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Router, Route } from 'react-router';
-import { Application, MovieListContainer, ShowListContainer, MovieScreen } from '../index';
+import { Application, MovieListContainer, ShowListContainer, ShowDetails, Screen } from '../index';
 
 class AppRouter extends React.Component {
 
@@ -15,8 +15,9 @@ class AppRouter extends React.Component {
       <Router {...this.props}>
         <Route component={Application}>
           <Route path="/" component={MovieListContainer} {...this.props} />
-          <Route path="/screen" component={MovieScreen} />
+          <Route path="/screen" component={Screen} />
           <Route path="/shows" component={ShowListContainer} {...this.props} />
+          <Route path="/show-details" component={ShowDetails} />
         </Route>
       </Router>
     );
